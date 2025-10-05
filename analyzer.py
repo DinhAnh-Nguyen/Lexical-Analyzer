1e687e97301db215c25dcb204efca89f576b8b7f
Ethan <157554972+ethanp4@users.noreply.github.com>
2025-10-04 21:21:48 -0600

    make ui and lexical analyzer

---
+++ analyzer.py
@@ -0,0 +1,56 @@
+import re
+from typing import Union, List, Tuple
+
+class LexicalAnalyzer:
+  def __init__(self):
+    # define regex rules for parsing
+    self.tokens = [
+      ('NUMBER',     r'\d+'), # a number of 1 or greater length
+      ('ASSIGN',     r'<-'),
+      ('LBRACKET',   r'\['),
+      ('RBRACKET',   r'\]'),
+      ('PLUS',       r'\+'),
+      ('MINUS',      r'-'),
+      ('TIMES',      r'\*'),
+      ('DIVIDE',     r'/'),
+      ('NEWLINE',    r'\n'),
+      ('COMMA',      r','),
+      ('KEYWORD',    r'\b(for|in|do)\b'), # match any defined keyword
+      ('IDENT',      r'[A-Za-z_]\w*'), #any string identifier
+      ('TAB',        r'\t'), #tabs are significant in language that doesnt use brackets for blocks
+      ('WHITESPACE', r'\s+'), #whitespaces are matched but skipped in the end
+      ('MISMATCH',   r'.') # anything that didnt match is a syntax error
+    ]
+    #join the regex rules into a single regex pattern
+    self.regex = re.compile(
+      '|'.join(f'(?P<{name}>{pattern})' for name, pattern in self.tokens)
+    )
+
+  #this function takes in a string of code and returns a list of tuples
+  def lex(self, code: str) -> Union[RuntimeError, List[Tuple[str, str]]]:
+    tokens = []
+    pos = 0
+
+    #iterate through input string
+    while pos < len(code):
+      #try to match it against regex
+      match = self.regex.match(code, pos)
+      if not match:
+        return RuntimeError(f'Unexpected character at position {pos}')
+      kind = match.lastgroup
+      value = match.group()
+      
+      if kind == 'SKIP':
+        pass
+      elif kind == 'MISMATCH':
+        return RuntimeError(f'Unexpected character: {value!r}')
+      # whitespaces arent syntactically significant, other than for separating tokens
+      elif kind == 'WHITESPACE':
+        pos = match.end()
+      else:
+        #if it matched, append it to the result array
+        tokens.append((kind, value))
+      #move the position forward
+      pos = match.end()
+    return tokens
+
---
+++ ui.py
@@ -0,0 +1,97 @@
+from tkinter import *
+import tkinter as tk
+
+from analyzer import LexicalAnalyzer
+
+class AnalyzerWindow(tk.Frame):
+	def __init__(self, master):
+		super().__init__(master)
+		#set the layout method to pack
+		self.pack()
+
+		#create variables
+		title = "Lexical Analyzer"
+
+		helpText = "This program is a small Lexical Analyzer. \nInput your " \
+		"code in the left textbox and then hit Analyze. The result will be in the right textbox" \
+		"\nThe text boxes can be resized via the handle in the middle"
+
+		self.defaultInputString = "input <- [5, 8, 12, 4, 10]" \
+		"\ncount <- 0" \
+		"\ntotal <- 0" \
+		"\nfor x in input do" \
+		"\n	count <- count + 1" \
+		"\n	total <- total + x " \
+		"\naverage <- total / count"
+
+		self.stringResult = tk.StringVar()
+		self.stringResult.set("Click Analyze!")
+		
+		self.backgroundColour = master["background"]
+
+		#create widgets
+		self.labelTitle = tk.Label(text=helpText)
+		self.labelTitle.pack()
+		self.labelTitle.configure(background=self.backgroundColour)
+
+		# create a pane to hold the two textareas that can be resized
+		# via a handle in the middle
+		self.paned = tk.PanedWindow(master, orient=tk.HORIZONTAL, sashrelief=tk.RIDGE, sashwidth=10)
+		#flat, groove, raised, ridge, solid, or sunken
+		self.paned.pack(fill=tk.BOTH, expand=True)
+		self.paned.configure(background=self.backgroundColour)
+
+		#create the two text areas with the pane as their parent
+		self.textInput = tk.Text(self.paned, height = 15, width = 40)
+		self.textInput.insert(INSERT, self.defaultInputString)
+		self.textInput.config(font=("Courier", 10), background="khaki")
+
+		self.textResult = tk.Text(self.paned, height=15, width = 40)
+		self.textResult.insert(INSERT, self.stringResult.get())
+
+		self.textResult.tag_configure("EVEN", background="medium spring green")
+		self.textResult.tag_configure("ODD", background="pale green")
+		self.textResult.config(font=("Courier", 10), background="pale green")
+		
+		#add the children to the pane
+		self.paned.add(self.textInput)
+		self.paned.add(self.textResult)
+
+		#create the buttons
+		self.buttonAnalyze = tk.Button(text = "Analyze!", command = self.analyze)
+		self.buttonAnalyze.pack()
+		self.buttonAnalyze.configure(background=self.backgroundColour)
+		
+		self.buttonExample = tk.Button(text = "Insert default code", command = self.setExampleInput)
+		self.buttonExample.pack()
+		self.buttonExample.configure(background=self.backgroundColour)
+
+	def setExampleInput(self):
+		self.textInput.delete(1.0, END)
+		self.textInput.insert(INSERT, self.defaultInputString)
+
+	#function to be called when clicking analyze
+	def analyze(self, event = None):
+		analyzer = LexicalAnalyzer()
+		result = analyzer.lex(self.textInput.get(1.0, END))
+		self.textResult.delete(1.0, END)
+
+		if not isinstance(result, RuntimeError):
+			# if its a valid result, then iterate through it in order to make it readable
+			for idx, token in enumerate(result):
+				kind, value = token
+				value = value.replace("\n", "\\n").replace("\t", "\\t")
+				line = f"{kind:<10} : {value}\n"
+				tag_name = "EVEN" if idx % 2 else "ODD"
+				self.textResult.insert(tk.END, line, (tag_name))
+		else:
+			self.textResult.insert(INSERT, result)
+
+root = tk.Tk()
+root.minsize(400, 300)
+# root.resizable(False, False)
+root.title("Lexical Analyzer")
+root.configure(background="azure3")
+
+analyzer = AnalyzerWindow(root)
+analyzer.mainloop()
